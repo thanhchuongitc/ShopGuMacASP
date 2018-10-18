3de12bcaf38eb1b1ec67e30939406bcfdd860c83
Trần Thanh Chương <clantczombie@gmail.com>
2018-10-18 11:19:45 +0700

    Delete Product

--- ShopGumac/Controllers/AdminController.cs
+++ ShopGumac/Controllers/AdminController.cs
@@ -1,4 +1,5 @@
 ﻿using MongoDB.Bson;
+using MongoDB.Driver;
 using ShopGumac.App_Start;
 using ShopGumac.Models;
 using System;
@@ -16,10 +17,43 @@ namespace ShopGumac.Controllers
         public AdminController() {
             client = new MongoContext();
         }
+        private bool checkLogin()
+        {
+            if (Session["user"] == null)
+                return true;
+            return false;
+        }
         public ActionResult ListProduct()
         {
+            if (checkLogin())
+                return RedirectToAction("Index","Index");
             var res = client.database.GetCollection<ProductModel>("products").FindAll().ToList();
             return View(res);
+        }
+
+        public ActionResult EditProduct(string id) {
+            if (checkLogin())
+                return RedirectToAction("Index", "Index");
+            if (String.IsNullOrEmpty(id))
+                return RedirectToAction("ListProduct");
+            var builder = Builders<BsonDocument>.Filter;
+            var filter = builder.Eq("_id",ObjectId.Parse(id));
+            var product = client._database.GetCollection<BsonDocument>("products").Find(filter).ToList();
+            ViewBag.product = product[0];
+            return View();
+        }
+        [HttpPost]
+        public void deleteProduct(string id)
+        {
+            var builder = Builders<BsonDocument>.Filter;
+            var filter = builder.Eq("_id", ObjectId.Parse(id));
+            client._database.GetCollection<BsonDocument>("products").FindOneAndDelete(filter);
+        }
+
+        public ActionResult partialType()
+        {
+
+            return View();
         }
     }
 }
