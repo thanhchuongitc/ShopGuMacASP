a5f1a177ce7e94dfe98448125f9cb041559bb39e
Trần Thanh Chương <clantczombie@gmail.com>
2018-10-18 08:37:36 +0700

    Product List Table

---
+++ ShopGumac/Controllers/AdminController.cs
@@ -0,0 +1,25 @@
+﻿using MongoDB.Bson;
+using ShopGumac.App_Start;
+using ShopGumac.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ShopGumac.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        MongoContext client;
+        public AdminController() {
+            client = new MongoContext();
+        }
+        public ActionResult ListProduct()
+        {
+            var res = client.database.GetCollection<ProductModel>("products").FindAll().ToList();
+            return View(res);
+        }
+    }
+}
